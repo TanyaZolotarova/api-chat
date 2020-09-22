f78a210aaa5f612e62e4493f265486555c4b3830
TanyaZolotarova <tstrelnikova.91@gmail.com>
2020-09-22 11:53:34 +0300

    fix config and name models

--- models/message.js
+++ models/message.js
@@ -1,24 +1,18 @@
 'use strict';
-const {
-    Model
-} = require('sequelize');
+
+const {Model} = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
     class Message extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
         static associate(models) {
             // define association here
         }
     };
-    Message.init({
 
+    Message.init({
         message: DataTypes.TEXT,
         user_id: DataTypes.INTEGER,
         chatroom_id: DataTypes.INTEGER,
-
     }, {
         sequelize,
         modelName: 'Message',
