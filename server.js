a18f86cc1f126b11e0078a093784ea5004c04d26
oa1cspace <oa1cspace@gmail.com>
2020-09-23 17:35:50 +0300

    feat: socketIO with system 'connection' and 'disconnection' events + custom event 'message';

--- server.js
+++ server.js
@@ -1,21 +1,38 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const app = express();
-const dotenv = require('dotenv');
-dotenv.config();
-
-app.use(bodyParser.json());
-app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
+const app = require('./app');
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
 
 
-app.get('/', (req, res) => {
-    res.json({message: 'welcome to application.'})
+// Listen port
+server.listen(process.env.PORT, () => {
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`);
 });
 
-// set port, listen for request
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)
-})
+// Array with connections
+connections = [];
+
+// == SYSTEM EVENT ==  Function, which runs when connecting client;
+io.sockets.on('connection', function (socket) {
+    console.log("Connected successfully");
+    // Adding new connection into array;
+    connections.push(socket);
+
+
+    // == CUSTOM EVENT ==  Function, receiving a message from any client;
+    socket.on('message', function ({name, message}) {
+        console.log('====[ send_message ]==========>', name +':', message);
+        // Inside of the function we sending an event 'add_message',
+        // which will show up a new message for all connected clients;
+        io.sockets.emit('add_message', {name, message});
+    });
+
+
+    // == SYSTEM EVENT ==  Function, which runs when client disconnected from server;
+    socket.on('disconnect', function (data) {
+        // Removing an user from array of 'connections';
+        connections.splice(connections.indexOf(socket), 1);
+        console.log("Disconnected");
+    });
+
+
+});
