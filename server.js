50b93f2317db033e380247fccb46a59594bc5f2b
TanyaZolotarova <tstrelnikova.91@gmail.com>
2020-09-23 18:39:46 +0300

    fix name files

--- server.js
+++ server.js
@@ -26,20 +26,4 @@ app.listen(PORT, () => {
 })
 
 
-// socket.io
-// const httpServer = require('http').createServer((req, res) => {
-//   // serve the index.html file
-//   res.setHeader('Content-Type', 'text/html');
-//   res.setHeader('Content-Length', Buffer.byteLength(content));
-//   res.end(content);
-// });
-//
-// const io = require('socket.io')(httpServer);
-//
-// io.on('connect', socket => {
-//   console.log('connect');
-// });
-//
-// httpServer.listen(3000, () => {
-//   console.log('go to http://localhost:3000');
-// });
+
