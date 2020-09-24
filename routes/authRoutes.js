2a3429ce252e2a99137a6ad36530c493b5b16ecb
TanyaZolotarova <tstrelnikova.91@gmail.com>
2020-09-24 19:43:41 +0300

    add https

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,21 +4,8 @@ const auth = require('../middleware/auth');
 const passport = require("passport");
 
 
-// router.get('/', users.findAll);
 router.post('/', authController.login);
-// router.delete('/:id', users.delete);
-// router.post('/logout', users.logout);
-// router.put('/:id/update', users.update);
-
 router.get('/me', auth, authController.me);
-
-// Google
-
-// router.get("/google/redirect",passport.authenticate("google"),(req,res)=>{
-//     res.send(req.user);
-//     console.log("authRoutes ", req.user)
-//     res.send("you reached the redirect URI");
-// });
 
 //app.get
 router.get("/logout", (req, res) => {
--- server.js
+++ server.js
@@ -3,16 +3,30 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const dotenv = require('dotenv');
+const fs = require('fs');
 dotenv.config();
+
 const authRouter = require('./routes/authRoutes');
-const server = require('http').createServer(app);
+
+const options = {
+    key: fs.readFileSync('./key.pem', 'utf8'),
+    cert: fs.readFileSync('./server.crt', 'utf8')
+};
+
+// const server = https.createServer(options, app);
+const server = require('https').createServer(options, app);
+
 const io = require('socket.io').listen(server);
+
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const authConfig = require ('./config/auth.config');
+const authConfig = require('./config/auth.config');
 const db = require('./models');
 const user = db.user;
 const cookieSession = require("cookie-session");
+const morgan = require("morgan");
+
+app.use(morgan('tiny'));
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -21,26 +35,23 @@ app.use(bodyParser.json());
 //google-authorization
 passport.use(
     new GoogleStrategy({
-      clientID: authConfig.google.clientID,
-      clientSecret: authConfig.google.clientSecret,
-      callbackURL: '/auth/google/redirect'
+        clientID: authConfig.google.clientID,
+        clientSecret: authConfig.google.clientSecret,
+        callbackURL: 'https://locahost:3000/auth/google/redirect'
     }, (accessToken, refreshToken, profile, done) => {
-        // passport callback function
-        //check if user already exists in our db with the given profile ID
-
         console.log('GoogleStrategy', profile);
+
         user.findOne({
-            where :{ googleId: profile.id}
+            where: {googleId: profile.id}
         }).then((currentUser) => {
             if (currentUser) {
-                //if we already have a record with the given profile ID
                 done(null, currentUser);
             } else {
                 //if not, create a new user
                 new user({
                     email: profile.emails[0].value,
                     googleId: profile.id,
-                    name: profile.name,
+                    name: `${profile.givenName} ${profile.familyName}`,
                 }).save().then((newUser) => {
                     done(null, newUser);
                 });
@@ -63,8 +74,8 @@ passport.deserializeUser((id, done) => {
 
 app.use(cookieSession({
     // milliseconds of a day
-    maxAge: 24*60*60*1000,
-    keys:[authConfig.session.cookieKey]
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [authConfig.session.cookieKey]
 }));
 
 app.use(passport.initialize());
@@ -73,14 +84,10 @@ app.use(passport.session());
 
 // Middle ware registration
 app.get('/', (req, res) => {     // req - all  res -
-  res.json({message: 'welcome to application.'})
+    res.json({message: 'welcome to application.'})
 });
 
 app.use('/auth', authRouter);
-
-app.get('/auth', (req, res)=> {
-    res.json({message: 'welcome to auth.'})
-})
 
 app.post("/auth/google",
     passport.authenticate('google', {
@@ -94,13 +101,11 @@ app.get("/auth/google", passport.authenticate('google', {
 
 app.get("/auth/google/redirect",passport.authenticate('google'), (req,res) =>{
     res.send(req.user);
-    res.send("you reached the redirect URI");
 });
-
 
 // Listen port
 server.listen(process.env.PORT, () => {
-  console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+    console.log(`Example app listening at https://localhost:${process.env.PORT}`);
 });
 
 // Array with connections
@@ -108,26 +113,26 @@ connections = [];
 
 // == SYSTEM EVENT ==  Function, which runs when connecting client;
 io.sockets.on('connection', function (socket) {
-  console.log("Connected successfully");
-  // Adding new connection into array;
-  connections.push(socket);
+    console.log("Connected successfully");
+    // Adding new connection into array;
+    connections.push(socket);
 
 
-  // == CUSTOM EVENT ==  Function, receiving a message from any client;
-  socket.on('message', function ({name, message}) {
-    console.log('====[ send_message ]==========>', name +':', message);
-    // Inside of the function we sending an event 'add_message',
-    // which will show up a new message for all connected clients;
-    io.sockets.emit('add_message', {name, message});
-  });
+    // == CUSTOM EVENT ==  Function, receiving a message from any client;
+    socket.on('message', function ({name, message}) {
+        console.log('====[ send_message ]==========>', name + ':', message);
+        // Inside of the function we sending an event 'add_message',
+        // which will show up a new message for all connected clients;
+        io.sockets.emit('add_message', {name, message});
+    });
 
 
-  // == SYSTEM EVENT ==  Function, which runs when client disconnected from server;
-  socket.on('disconnect', function (data) {
-    // Removing an user from array of 'connections';
-    connections.splice(connections.indexOf(socket), 1);
-    console.log("Disconnected");
-  });
+    // == SYSTEM EVENT ==  Function, which runs when client disconnected from server;
+    socket.on('disconnect', function (data) {
+        // Removing an user from array of 'connections';
+        connections.splice(connections.indexOf(socket), 1);
+        console.log("Disconnected");
+    });
 
 
 });
